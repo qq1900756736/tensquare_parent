af80b674536ae90b7775634d22162465e8448aba
dong <1900756736@qq.com>
2019-01-27 10:08:21 +0800

    1

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -1,23 +1,54 @@
 package com.tensquare.base.controller;
 
+import cn.hutool.core.util.StrUtil;
+import com.tensquare.base.pojo.Label;
+import com.tensquare.base.service.LabelService;
 import entity.Result;
 import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/label")
 public class LabelController {
+    @Autowired
+    private LabelService labelService;
+
     @GetMapping
-    public Result findAll(){
-        return  new Result(true, StatusCode.OK,"");
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
     }
 
     @GetMapping("/{id}")
-    public Result findById(@PathVariable String id){
-        return  new Result(true, StatusCode.OK,"");
+    public Result findById(@PathVariable String id) {
+        return new Result(true, StatusCode.OK, "查询成功",labelService.findById(id));
     }
 
+    @PostMapping()
+    public Result save(@RequestBody Label label) {
+        labelService.save(label);
+        return new Result(true, StatusCode.OK, "添加成功");
+    }
+
+    @PutMapping("/{id}")
+    public Result update(@PathVariable String id, @RequestBody Label label) {
+        label.setId(id);
+        labelService.update(label);
+        return new Result(true, StatusCode.OK, "更新成功");
+    }
+
+    @DeleteMapping("/{id}")
+    public Result deleteById(@PathVariable String id) {
+        labelService.deleteById(id);
+        return new Result(true, StatusCode.OK, "");
+    }
+
+    @PostMapping("search")
+    public Result findSearch(@RequestBody Label label){
+
+        return new Result(true,StatusCode.OK,"查询成功",labelService.findSerch());
+    }
 }
 
 
---
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -0,0 +1,79 @@
+package com.tensquare.base.service;
+
+import cn.hutool.core.util.StrUtil;
+import com.tensquare.base.dao.LabelDao;
+import com.tensquare.base.pojo.Label;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import util.IdWorker;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Transactional
+public class LabelService {
+    @Autowired
+    private LabelDao labelDao;
+
+    @Autowired
+    private IdWorker idWorker;
+
+    public List<Label> findAll() {
+        return labelDao.findAll();
+    }
+
+
+    public Label findById(String id) {
+        return labelDao.findById(id).get();
+    }
+
+
+    public void save(Label label) {
+        label.setId(idWorker.nextId() + "");
+        labelDao.save(label);
+    }
+
+
+    public void update(Label label) {
+        labelDao.save(label);
+    }
+
+
+    public void deleteById(String id) {
+        labelDao.deleteById(id);
+    }
+
+    /**
+     *
+     * @param root  根对象,把条件封装到哪个对象,where 类名=label.getid
+     * @param query 查询关键字
+     * @param cb 用来封装条件对象的
+     * @return
+     */
+    public List<Label> findSerch(Label label) {
+        return labelDao.findAll(new Specification<Label>() {
+
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                List<Predicate> list = new ArrayList<>();
+                if (StrUtil.isNotBlank(label.getLabelname())) {
+                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                    list.add(predicate);
+                }
+                if (StrUtil.isNotBlank(label.getState())) {
+                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());
+                    list.add(predicate);
+                }
+                return cb.and(list.toArray(new Predicate[list.size()]));//对都条件and连接
+            }
+        });
+    }
+}
